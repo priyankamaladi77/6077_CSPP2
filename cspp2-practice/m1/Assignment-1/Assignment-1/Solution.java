f374c3b955f4928bb60c477ebde2688b317df599
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-27 20:47:50 +0530

    name

---
+++ cspp2-practice/m1/Assignment-1/Assignment-1/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * @author PriyankaMaladi.
+ */
+public final class Solution {
+    /**
+     * Empty constructor.
+     */
+    private Solution() {
+        // I am not using this constructor.
+    }
+    /**
+     * main function to print name and roll number.
+     * @param args is the parameter for this function.
+     */
+    public static void main(final String[] args) {
+        String name = "priyanka";
+        long num = 20186077;
+        System.out.println("Name :" + name + " " + "Roll Number :" + num);
+    }
+}
