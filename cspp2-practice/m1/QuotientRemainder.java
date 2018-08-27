68489a6ff211f858bb29ef0547e7c28d86d61192
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-27 19:42:53 +0530

    QuotientRemainder

---
+++ cspp2-practice/m1/QuotientRemainder.java
@@ -0,0 +1,20 @@
+/**
+ * @author PriyankaMaladi.
+ */
+public final class QuotientRemainder {
+    /**
+     * Empty constructor.
+     */
+    private QuotientRemainder() {
+        // I am not using this constructor
+    }
+    /**
+     * main function to find the Quotient and remainder
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(a/b + " " + a%b);
+    }
+}
