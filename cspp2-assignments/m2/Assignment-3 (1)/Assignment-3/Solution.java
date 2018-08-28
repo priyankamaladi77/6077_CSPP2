5a6500bee01ecf36725989beb4857fa5fe86116a
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-28 18:42:29 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3 (1)/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3 (1)/Assignment-3/Solution.java
@@ -6,15 +6,27 @@ public final class Solution {
     /*
     Do not modify this main function.
     */
+    private Solution() {
+        // I am not using this constructor
+    }
+    /**
+     * main function to find the roots of quadratic equation.
+     * @param args is the parameter for this function
+     */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
         System.out.println(result);
     }
-    /*
+    /**
     Need to write the power function and print the output.
+    
+    @param      b     { parameter_description }
+    @param      e     { parameter_description }
+    
+    @return     { description_of_the_return_value }
     */
     public static long power(final int b, final int e) {
         if (e > 0) {
