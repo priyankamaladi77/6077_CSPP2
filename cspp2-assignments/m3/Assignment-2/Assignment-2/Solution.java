e64bc1cf82ea48882817241ce0104108650b0561
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-29 15:40:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Assignment-2/Solution.java
@@ -1,34 +1,36 @@
 import java.util.Scanner;
 /**
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 public class Solution {
-/* Fill the main function to print the number of 7's between 1 to n*/
+/**
+ * Fill the main function to print the number of 7's between 1 to n
+ * @param args The arguments
+ */
     public static void main(final String[] args) {
-
-        Scanner s=new Scanner(System.in);
-        int n = s.nextInt();
-        int i = 0, count = 0, value = 0;
-        while (i < n) {
-        	if (i % 10 == 7) {
-        		count += 1;
-        	}
-        	 i += 1;
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int i = 0, count = 0, val = 0;
+        while (i < num) {
+            if (i % 10 == 7) {
+                count += 1;
+            }
+             i += 1;
         }
-        for (int j = 0; j < n; j++) {
-        	int a = j / 10;
-        	while (a > 0) {
-        		if (a % 10 == 7) {
-        			value += 1;
-        		}
-        		a = a / 10;
-        	}
-        	if (a == 7) {
-        		value += 1;
-        	}
-        	
+        for (int j = 0; j < num; j++) {
+            int a = j / 10;
+            while (a > 0) {
+                if (a % 10 == 7) {
+                    val += 1;
+                }
+                a = a / 10;
+            }
+            if (a == 7) {
+                val += 1;
+            }
+            
         }
-        int result = count + value;
-        System.out.println(result);
+        int res = count + val;
+        System.out.println(res);
     }
 }
