656e762783ad432c3297a52e6cff05f3254dc869
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-29 17:42:40 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -6,19 +6,19 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int res = gcd(n1,n2);
+        int res = gcd(n1, n2);
         System.out.println(res);
     }
     /*
     Need to write the gcd function and print the output.
     */
-    public static int gcd(final int n1, int n2) {
+    public static int gcd(final int n1, final int n2) {
         int gcd = 1;
         for (int i = 1; i <= n1 && i <= n2; i++) {
-            if(n1 % i == 0 && n2 % i == 0) {
+            if (n1 % i == 0 && n2 % i == 0) {
                 gcd = i;
             }
         }
