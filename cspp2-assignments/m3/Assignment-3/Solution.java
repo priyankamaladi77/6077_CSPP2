5cbd3e63b3d461e50d5427d7fa587e353f914448
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-29 17:50:29 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
     Do not modify this main function.
     */
-public class Solution {
+public final class Solution {
      /**
     Do not modify this main function.
     */
