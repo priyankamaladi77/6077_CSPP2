4eb7a6a0b178c33c134c2479070e0df19a444667
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-30 20:57:33 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+/**
+ * @author PriyankaMaladi.
+ */
+public final class Solution {
+     /**
+     * Constructs the object.
+     */
+    private Solution() {
+    	//Empty Constructor.
+        // I am not using this constructor.
+    }
+    /**
+     * Fill the main function to print resultant of addition of matrices
+     * main function to add matrices of same size.
+     * @param args is the parameter for this function.
+     */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int[][] mat1 = createMatrix(sc);
+        int[][] mat2 = createMatrix(sc);
+        sc.close();
+        int m = getRowSize(mat1), n = getColumnSize(mat1);
+        int a = getRowSize(mat2), b = getColumnSize(mat2);
+        if ((m != a) || (n != b)) {
+        	System.out.println("matrices are diffrent in size");
+        } else {
+        	printMatrix(getAdd(mat1, mat2, createMatrix(m, n)));
+        }
+	}
+	/**
+     * { function_description }.
+     *
+     * @param addMatrix The sum matrix
+     */
+	public static void printMatrix(final int[][] addMatrix) {
+		for (int i = 0; i < getRowSize(addMatrix); i++) {
+			for (int j = 0; j < getColumnSize(addMatrix) - 1; j++) {
+				System.out.println(addMatrix[i][j] + " ");
+			}
+			System.out.println(addMatrix[i][getRowSize(addMatrix) - 1]);
+		}
+	}
+	/**
+     * Gets the add.
+     * @param mat1 The matrix 1
+     * @param mat2 The matrix 2
+     * @param addMatrix The sum matrix
+     * @return The add.
+     */
+	public static int[][] getAdd(final int[][] mat1,
+                                final int[][] mat2,
+                                final int[][] addMatrix) {
+		for (int i = 0; i < getRowSize(mat1); i++) {
+			for (int j = 0; j < getColumnSize(mat1); j++) {
+                addMatrix[i][j] = mat1[i][j] + mat2[i][j];
+			}
+		}
+		return addMatrix;
+	}
+	/**
+     * { function_description }.
+     * @param sc The scanner object
+     * @return { description_of_the_return_value }
+     */
+    public static int[][] createMatrix(final Scanner sc) {
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arrays = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arrays[i][j] = sc.nextInt();
+            }
+        }
+        return arrays;
+    }
+
+   /**
+     * Creates a matrix.
+     *
+     * @param      m     { parameter_description }
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int[][] createMatrix(final int m, final int n) {
+        return new int[m][n];
+    }
+
+    /**
+     * Gets the row size.
+     *
+     * @param      arrays  The array
+     *
+     * @return     The row size.
+     */
+    public static int getRowSize(final int[][] arrays) {
+        return arrays.length;
+    }
+    /**
+     * Gets the column size.
+     *
+     * @param      array  The array
+     *
+     * @return     The column size.
+     */
+    public static int getColumnSize(final int[][] arrays) {
+        return arrays[0].length;
+    }
+}
