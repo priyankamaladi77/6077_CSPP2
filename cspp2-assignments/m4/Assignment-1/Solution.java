a32ab423c53dacad54085a69b1629265272bf51e
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-30 19:56:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
  * @author PriyankaMaladi.
  */
 public final class Solution {
-	/**
-	Fill this main function to print maximum of given array.
-	*/
-	private Solution() {
+    /**
+    Fill this main function to print maximum of given array.
+    */
+    private Solution() {
         // I am not using this constructor.
     }
     /**
      * main function to find the average of given elements in array.
      * @param args is the parameter for this function.
      */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int max = 0;
-		int[] arrays = new int[num];
-		for (int i = 0; i < num; i++) {
-			arrays[i] = sc.nextInt();
-			if (arrays[i] > max) {
-				max = arrays[i];
-			}
-		}
-		System.out.println(max);
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int max = 0;
+        int[] arrays = new int[num];
+        for (int i = 0; i < num; i++) {
+            arrays[i] = sc.nextInt();
+            if (arrays[i] > max) {
+                max = arrays[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
