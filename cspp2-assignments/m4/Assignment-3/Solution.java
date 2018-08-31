1e82027641e6e924b11a371610ea9eb32e45b1d0
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-31 16:40:36 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -0,0 +1,33 @@
+
+// Given an String, Write a java method that returns the decimal value for the given binary string.
+
+import java.util.Scanner;
+public final class Solution {
+/**
+ *Do not modify this main function.
+ *@param args The arguments
+ */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i <= n; i++) {
+			String s = sc.nextLine();
+			long res = binaryToDecimal(s);//Write binaryToDecimal function
+			System.out.println(res);
+		}
+	}
+	public static long binaryToDecimal(final String s) {
+		double result;
+		int sum = 0;
+		int base = 2;
+		int j = s.length() - 1;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '1') {
+			result = Math.pow(2, j);
+			sum += result;
+			}
+			j = j - 1;
+		}
+		return sum;
+	}
+}
