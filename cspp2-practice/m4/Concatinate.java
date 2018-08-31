a5c3c00e54c8d3ccb53899d119b0b62c97e896c5
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-31 14:31:43 +0530

    Concatinate

---
+++ cspp2-practice/m4/Concatinate.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * @author PriyankaMaladi.
+ */
+public final class Concatinate {
+    /**
+    Do not modify this main function.
+    */
+    private Concatinate() {
+        // I am not using this constructor
+    }
+    /**
+     * main function to find the average of given elements in array.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String result = s.next();
+        System.out.println("Hello" + " " + result + "!");
+    }
+}
