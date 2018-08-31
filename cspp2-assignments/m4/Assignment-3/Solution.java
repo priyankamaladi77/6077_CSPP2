3a3ca29a4c0baa4b47a2ce616bc6276a71102372
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-31 16:47:07 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,4 +1,6 @@
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+/*Given an String, Write a java method that returns
+ *the decimal value for the given binary string.
+ */
 /**
  * @author PriyankaMaladi.
  */
@@ -8,27 +10,32 @@ public final class Solution {
  *Do not modify this main function.
  *@param args The arguments
  */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i = 0; i < n; i++) {
-			String s = sc.next();
-			long res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static long binaryToDecimal(final String s) {
-		double result;
-		int sum = 0;
-		int base = 2;
-		int j = s.length() - 1;
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '1') {
-			result = Math.pow(2, j);
-			sum += result;
-			}
-			j = j - 1;
-		}
-		return sum;
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            long res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }
+     * @param s { parameter_description }
+     * @return { description_of_the_return_value }
+     */
+    public static long binaryToDecimal(final String s) {
+        double result;
+        int sum = 0;
+        int base = 2;
+        int j = s.length() - 1;
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+            result = Math.pow(2, j);
+            sum += result;
+            }
+            j = j - 1;
+        }
+        return sum;
+    }
 }
