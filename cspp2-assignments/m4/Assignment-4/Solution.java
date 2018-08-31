79856b30c2e8af867beaf3a0f18338b6e368e73f
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-31 14:57:34 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -10,12 +10,15 @@ public class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		String actual = sc.nextLine();
-		String reverse = reverseString(actual);	
+		String reverse = reverseString(actual);
 		System.out.println(reverse);
-		
 	}
-	//Write reverseString function
-	public static String reverseString(String actual) {
+	/**
+	 * Write reverseString function
+	 * @param actual The actual
+	 * @return { description_of_the_return_value }
+	*/
+	public static String reverseString(final String actual) {
 		String result = "";
 		for (int i = 0; i < actual.length(); i++) {
 			result = actual.charAt(i) + result;
