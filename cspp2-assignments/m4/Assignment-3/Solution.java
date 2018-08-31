5b1c7839f8d6704225c520cd654698ecb1d6223a
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-31 16:50:24 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,4 +1,4 @@
-/*Given an String, Write a java method that returns
+/*Given an String, Write a java method that returns.
  *the decimal value for the given binary string.
  */
 /**
@@ -6,30 +6,36 @@
  */
 import java.util.Scanner;
 public final class Solution {
-/**
- *Do not modify this main function.
- *@param args The arguments
- */
+    /**
+    *Do not modify this main function.
+    */
+    private Solution() {
+        // I am not using this constructor.
+    }
+    /**
+     * main function to covert binary to decimal value.
+     * @param args is the parameter for this function
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String s = sc.next();
             long res = binaryToDecimal(s);
             System.out.println(res);
         }
     }
     /**
-     * { function_description }
-     * @param s { parameter_description }
-     * @return { description_of_the_return_value }
+     * { function_description }.
+     * @param s { parameter_description }.
+     * @return { description_of_the_return_value }.
      */
     public static long binaryToDecimal(final String s) {
         double result;
         int sum = 0;
         int base = 2;
         int j = s.length() - 1;
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
             result = Math.pow(2, j);
             sum += result;
