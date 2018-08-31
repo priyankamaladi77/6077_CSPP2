a907d2c3da9e446746fc34a8f819c3d44f5da96d
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-31 15:02:29 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -2,27 +2,30 @@
  * @author PriyankaMaladi.
  */
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String actual = sc.nextLine();
-		String reverse = reverseString(actual);
-		System.out.println(reverse);
-	}
-	/**
-	 * Write reverseString function
-	 * @param actual The actual
-	 * @return { description_of_the_return_value }
-	*/
-	public static String reverseString(final String actual) {
-		String result = "";
-		for (int i = 0; i < actual.length(); i++) {
-			result = actual.charAt(i) + result;
-		}
-		return result;
-	}
+public class Solution {
+    /**
+     * Do not modify the main function
+     * @param args The arguments
+    */
+    private Solution() {
+        // I am not using this constructor
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String actual = sc.nextLine();
+        String reverse = reverseString(actual);
+        System.out.println(reverse);
+    }
+    /**
+     * Write reverseString function.
+     * @param actual The actual.
+     * @return { description_of_the_return_value }.
+    */
+    public static String reverseString(final String actual) {
+        String result = "";
+        for (int i = 0; i < actual.length(); i++) {
+            result = actual.charAt(i) + result;
+        }
+        return result;
+    }
 }
