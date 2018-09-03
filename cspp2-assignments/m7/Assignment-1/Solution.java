eb5090323aca4a9ea17eb4e16510f0c903458e84
priyankamaladi77 <svanmp@msitprogram.net>
2018-09-03 23:03:09 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+/**
+ * @author     : priyanka maladi
+ */
+class InputValidator
+{
+	/*
+	 * Write the atrributes and methods for InputValidator
+	 */
+	private String input;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      str   The string
+	 */
+	InputValidator(final String str) {
+		this.input = str;
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	boolean validateData() {
+		int len = input.length();
+		final int length = 6;
+		if (len >= length) {
+			return true;
+		}
+		return false;
+	}
+}
+/**
+ * { item_description }
+ */
+public final class Solution {
+	/**
+	 * { item_description }
+	 */
+	public static void main(String args[]) {
+    	Scanner s=new Scanner(System.in);
+    	String input=s.next();
+    	InputValidator i=new InputValidator(input);    	
+    	System.out.println(i.validateData());
+    }
+}
