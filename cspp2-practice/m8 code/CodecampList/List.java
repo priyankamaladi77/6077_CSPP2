fcb8112cac3523384bac5b0b4571323f6ed416cb
priyankamaladi77 <svanmp@msitprogram.net>
2018-09-04 17:24:21 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-practice/m8 code/CodecampList/List.java
+++ cspp2-practice/m8 code/CodecampList/List.java
@@ -62,6 +62,9 @@ public class List {
      * The purpose of the constructor is to initialize the class variables with
      * some default values.
      */
+    /**
+     * @variable TEN.
+     */
     private static final int TEN = 10;
     public List() {
 
