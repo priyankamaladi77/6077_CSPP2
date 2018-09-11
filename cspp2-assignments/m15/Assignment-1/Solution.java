9ec7508bcab28216c55a15a4cf676873088f844d
priyankamaladi77 <svanmp@msitprogram.net>
2018-09-11 13:54:20 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/Solution.java
+++ cspp2-assignments/m15/Assignment-1/Solution.java
@@ -133,9 +133,9 @@ public class Solution {
     		throw new IndexOutofBoundsException("Index Out of Bounds Exception");
     	} if (start == end) {
     		return newlist;
-    	} else if (start < 0 || end < 0 || start > list.length || end > list.length) {
+    	} else if (start < 0 && end < 0 || start > list.length || end > list.length) {
     		throw new IndexOutofBoundsException("Index Out of Bounds Exception");
-    	} else if ((start >= 0 && start <= end) && (end > 0 && end > start && end < list.length)) {
+    	} else if ((start >= 0 && start < end) && (end > 0 && end > start && end < list.length)) {
     		int k = 0;
     		for (k = start; k < end; k++) {
     			newlist.add(list[k]);
