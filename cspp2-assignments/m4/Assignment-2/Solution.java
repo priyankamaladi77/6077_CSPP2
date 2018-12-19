5f797c47b6be38f83aed7d13d525e25d08223a45
priyankamaladi77 <svanmp@msitprogram.net>
2018-12-19 09:51:58 +0530

    "m4_a2: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,47 +1,46 @@
 import java.util.Scanner;
-/**
- * @author PriyankaMaladi.
- */
-public final class Solution {
-     /**
-     * Constructs the object.
-     */
-    private Solution() {
-        //Empty Constructor.
-        // I am not using this constructor.
+ 
+class Solution {
+    Solution() {
+        //
     }
-    /**
-     * Fill the main function to print resultant of addition of matrices
-     * main function to add matrices of same size.
-     * @param args is the parameter for this function.
-     */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int [][] mat1 = new int[m][n];
-        int [][] mat2 = new int[m][n];
-        int [][] add = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mat1[i][j] = sc.nextInt();
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mat2[i][j] = sc.nextInt();
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                add[i][j] = mat1[i][j] + mat2[i][j];
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.println(add[i][j]);
-            }
-            System.out.println();
-        }
-    }
+   public static void main(String args[])
+   {
+      int m, n, c, d;
+      Scanner in = new Scanner(System.in);
+     
+      System.out.println("Enter the number of rows and columns of matrix");
+      m = in.nextInt();
+      n  = in.nextInt();
+     
+      int first[][] = new int[m][n];
+      int second[][] = new int[m][n];
+      int sum[][] = new int[m][n];
+     
+      System.out.println("Enter the elements of first matrix");
+     
+      for (c = 0; c < m; c++)
+         for (d = 0; d < n; d++)
+            first[c][d] = in.nextInt();
+           
+      System.out.println("Enter the elements of second matrix");
+     
+      for (c = 0 ; c < m ; c++)
+         for (d = 0 ; d < n ; d++)
+            second[c][d] = in.nextInt();
+           
+      for (c = 0; c < m; c++)
+         for (d = 0; d < n; d++)
+             sum[c][d] = first[c][d] + second[c][d];  //replace '+' with '-' to subtract matrices
+ 
+      System.out.println("Sum of the matrices:");
+     
+      for (c = 0; c < m; c++)
+      {
+         for (d = 0; d < n; d++)
+            System.out.print(sum[c][d]+"\t");
+         
+         System.out.println();
+      }
+   }
 }
